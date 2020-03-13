6016303945ee0ca35f1cad0c8d8f9fd85e53c2f7
sach1n1 <sachin.s.dominic@gmail.com>
2020-03-13 17:29:39 +0100

    Add files via upload

---
+++ Loudness.cpp
@@ -0,0 +1,72 @@
+//Connect to analog port A0
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include "bmm150.h"
+#include "bmm150_defs.h"
+
+
+const char* ssid = "FIWARE";
+const char* password =  "!FIWARE!on!air!";
+#define mqtt_server "192.168.20.185"
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    // If you do not want to use a username and password, change next line to
+    // if (client.connect("ESP8266Client")) {
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+ 
+bool checkBound(float newValue, float prevValue, float maxDiff) {
+  return !isnan(newValue) &&
+         (newValue < prevValue - maxDiff || newValue > prevValue + maxDiff);
+}
+
+
+int loudness;
+
+void setup()
+{
+pinMode(15, OUTPUT);
+  digitalWrite(15, 1);
+  Serial.begin(9600);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+  client.setServer(mqtt_server, 1883);
+}
+
+void loop()
+{
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+    loudness = analogRead(0);
+    client.publish("FIWARE/Wio_Link/Magnet",String(loudness).c_str(),true);
+    Serial.println(loudness);
+    delay(1000);
+}
