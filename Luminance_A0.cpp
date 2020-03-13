34c8b1726fb23dba5180b8c6e0037be104f9defc
sach1n1 <sachin.s.dominic@gmail.com>
2020-03-13 17:25:25 +0100

    Add files via upload

---
+++ Luminance_A0.cpp
@@ -0,0 +1,142 @@
+//Connect to analog port of Wio Link board
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+float VoutArray[] =  { 0.0011498,  0.0033908,   0.011498, 0.041803,0.15199,     0.53367, 1.3689,   1.9068,  2.3};
+float  LuxArray[] =  { 1.0108,     3.1201,  9.8051,   27.43,   69.545,   232.67,  645.11,   73.52,  1000};
+
+const char* ssid = "FIWARE";
+const char* password =  "!FIWARE!on!air!";
+#define mqtt_server "192.168.20.185"
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    // If you do not want to use a username and password, change next line to
+    // if (client.connect("ESP8266Client")) {
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+ 
+bool checkBound(float newValue, float prevValue, float maxDiff) {
+  return !isnan(newValue) &&
+         (newValue < prevValue - maxDiff || newValue > prevValue + maxDiff);
+}
+
+float readAPDS9002Vout(uint8_t analogpin)
+{
+    // MeasuredVout = ADC Value * (Vcc / 1023) * (3 / Vcc)
+    // Vout samples are with reference to 3V Vcc
+    // The above expression is simplified by cancelling out Vcc
+    float MeasuredVout = analogRead(A0) * (3.0 / 1023.0);
+    //Above 2.3V , the sensor value is saturated
+
+    return MeasuredVout;
+
+}
+
+float FmultiMap(float val, float * _in, float * _out, uint8_t size)
+{
+    // take care the value is within range
+    // val = constrain(val, _in[0], _in[size-1]);
+    if (val <= _in[0]) return _out[0];
+    if (val >= _in[size-1]) return _out[size-1];
+
+    // search right interval
+    uint8_t pos = 1;  // _in[0] allready tested
+    while(val > _in[pos]) pos++;
+
+    // this will handle all exact "points" in the _in array
+    if (val == _in[pos]) return _out[pos];
+
+    // interpolate in the right segment for the rest
+    return (val - _in[pos-1]) * (_out[pos] - _out[pos-1]) / (_in[pos] - _in[pos-1]) + _out[pos-1];
+}
+
+float readLuminance(uint8_t analogpin)
+{
+
+    // MeasuredVout = ADC Value * (Vcc / 1023) * (3 / Vcc)
+    // Vout samples are with reference to 3V Vcc
+    // The above expression is simplified by cancelling out Vcc
+    float MeasuredVout = analogRead(A0) * (3.0 / 1023.0);
+    float Luminance = FmultiMap(MeasuredVout, VoutArray, LuxArray, 9);
+
+    /**************************************************************************
+
+    The Luminance in Lux is calculated based on APDS9002 datasheet -- > Graph 1
+    ( Output voltage vs. luminance at different load resistor)
+    The load resistor is 1k in this board. Vout is referenced to 3V Vcc.
+
+    The data from the graph is extracted using WebPlotDigitizer
+    http://arohatgi.info/WebPlotDigitizer/app/
+
+    VoutArray[] and LuxArray[] are these extracted data. Using MultiMap, the data
+    is interpolated to get the Luminance in Lux.
+
+    This implementation uses floating point arithmetic and hence will consume
+    more flash, RAM and time.
+
+    The Luminance in Lux is an approximation and depends on the accuracy of
+    Graph 1 used.
+
+    ***************************************************************************/
+
+    return Luminance;
+}
+
+
+//This code uses MultiMap implementation from http://playground.arduino.cc/Main/MultiMap
+
+
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    pinMode(15,OUTPUT);
+    digitalWrite(15,HIGH);
+    WiFi.begin(ssid, password);
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+  client.setServer(mqtt_server, 1883);
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+     if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  Serial.println("   ");
+  
+  delay(1000);
+    Serial.print("Vout =");
+    Serial.print(readAPDS9002Vout(A0));
+    Serial.print(" V,Luminance =");
+    Serial.print(readLuminance(A0));
+    Serial.println("Lux");
+    float lum = readLuminance(A0);
+    client.publish("FIWARE/Wio_Link/Magnet",String(lum).c_str(),true);
+    delay(500);
+}
