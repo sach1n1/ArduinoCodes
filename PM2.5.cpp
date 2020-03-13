64f6d79c5a5a1947840ddf073d6ccd4d94e59826
sach1n1 <sachin.s.dominic@gmail.com>
2020-03-13 17:31:51 +0100

    Add files via upload

---
+++ PM2.5.cpp
@@ -0,0 +1,153 @@
+/*
+ * Blink
+ * Turns on an LED on for one second,
+ * then off for one second, repeatedly.
+ */
+
+//Connect to I2C port of wio link
+
+#include "Arduino.h"
+
+#include "Seeed_HM330X.h"
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+
+const char* ssid = "FIWARE";
+const char* password =  "!FIWARE!on!air!";
+#define mqtt_server "192.168.20.185"
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+#ifdef  ARDUINO_SAMD_VARIANT_COMPLIANCE
+#define SERIAL_OUTPUT SerialUSB
+#else
+#define SERIAL_OUTPUT Serial
+#endif
+
+HM330X sensor;
+uint8_t buf[30];
+
+
+const char *str[] = {"sensor num: ", "PM1.0 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
+                     "PM2.5 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
+                     "PM10 concentration(CF=1,Standard particulate matter,unit:ug/m3): ",
+                     "PM1.0 concentration(Atmospheric environment,unit:ug/m3): ",
+                     "PM2.5 concentration(Atmospheric environment,unit:ug/m3): ",
+                     "PM10 concentration(Atmospheric environment,unit:ug/m3): ",
+};
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    // If you do not want to use a username and password, change next line to
+    // if (client.connect("ESP8266Client")) {
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+bool checkBound(float newValue, float prevValue, float maxDiff) {
+  return !isnan(newValue) &&
+         (newValue < prevValue - maxDiff || newValue > prevValue + maxDiff);
+}
+
+HM330XErrorCode print_result(const char *str, uint16_t value) {
+    if (NULL == str) {
+        return ERROR_PARAM;
+    }
+    SERIAL_OUTPUT.print(str);
+    SERIAL_OUTPUT.println(value);
+    return NO_ERROR;
+}
+
+/*parse buf with 29 uint8_t-data*/
+HM330XErrorCode parse_result(uint8_t *data) {
+    uint16_t value = 0;
+    if (NULL == data)
+        return ERROR_PARAM;
+    for (int i = 1; i < 8; i++) {
+        value = (uint16_t) data[i * 2] << 8 | data[i * 2 + 1];
+        
+        print_result(str[i - 1], value);
+        client.publish("FIWARE/Wio_Link/Magnet",String(value).c_str(),true);
+    }
+
+    return NO_ERROR;
+}
+
+HM330XErrorCode parse_result_value(uint8_t *data) {
+    if (NULL == data) {
+        return ERROR_PARAM;
+    }
+    for (int i = 0; i < 28; i++) {
+        SERIAL_OUTPUT.print(data[i], HEX);
+        SERIAL_OUTPUT.print("  ");
+        if ((0 == (i) % 5) || (0 == i)) {
+            SERIAL_OUTPUT.println("");
+        }
+    }
+    uint8_t sum = 0;
+    for (int i = 0; i < 28; i++) {
+        sum += data[i];
+    }
+    if (sum != data[28]) {
+        SERIAL_OUTPUT.println("wrong checkSum!!!!");
+    }
+    SERIAL_OUTPUT.println("");
+    return NO_ERROR;
+}
+
+
+/*30s*/
+void setup() {
+    SERIAL_OUTPUT.begin(9600);
+    pinMode(15,OUTPUT);
+    digitalWrite(15,HIGH);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+  client.setServer(mqtt_server, 1883);
+    
+    delay(100);
+    SERIAL_OUTPUT.println("Serial start");
+    if (sensor.init()) {
+        SERIAL_OUTPUT.println("HM330X init failed!!!");
+        while (1);
+    }
+
+}
+
+
+void loop() {
+    if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+    if (sensor.read_sensor_value(buf, 29)) {
+        SERIAL_OUTPUT.println("HM330X read result failed!!!");
+    }
+    parse_result_value(buf);
+    parse_result(buf);
+    SERIAL_OUTPUT.println("");
+
+    
+    delay(1000);
+}
