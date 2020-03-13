f9aed8fa41ae5b0a632ce1a562c83b4b877099ed
sach1n1 <sachin.s.dominic@gmail.com>
2020-03-13 17:41:12 +0100

    Add files via upload

---
+++ compass.cpp
@@ -0,0 +1,102 @@
+//Connect to I2C port of Wio Link
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include "bmm150.h"
+#include "bmm150_defs.h"
+
+BMM150 bmm = BMM150();
+
+const char* ssid = "FIWARE";
+const char* password =  "!FIWARE!on!air!";
+#define mqtt_server "192.168.20.185"
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    // If you do not want to use a username and password, change next line to
+    // if (client.connect("ESP8266Client")) {
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+ 
+bool checkBound(float newValue, float prevValue, float maxDiff) {
+  return !isnan(newValue) &&
+         (newValue < prevValue - maxDiff || newValue > prevValue + maxDiff);
+}
+
+void setup()
+{
+  pinMode(15, OUTPUT);
+  digitalWrite(15, 1);
+  Serial.begin(9600);
+  Wire.begin(4,5);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+  client.setServer(mqtt_server, 1883);
+  pinMode(13, INPUT);
+
+//Code for compass initialization
+  if(bmm.initialize() == BMM150_E_ID_NOT_CONFORM) {
+    Serial.println("Chip ID can not read!");
+    while(1);
+  } else {
+    Serial.println("Initialize done!");
+  }
+}
+
+void loop()
+{
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  Serial.println("   ");
+
+  bmm150_mag_data value;
+  bmm.read_mag_data();
+
+  value.x = bmm.raw_mag_data.raw_datax;
+  value.y = bmm.raw_mag_data.raw_datay;
+  value.z = bmm.raw_mag_data.raw_dataz;
+
+  float xyHeading = atan2(value.x, value.y);
+  float zxHeading = atan2(value.z, value.x);
+  float heading = xyHeading;
+
+  if(heading < 0)
+    heading += 2*PI;
+  if(heading > 2*PI)
+    heading -= 2*PI;
+  float headingDegrees = heading * 180/M_PI;
+  float xyHeadingDegrees = xyHeading * 180 / M_PI;
+  float zxHeadingDegrees = zxHeading * 180 / M_PI;
+
+  Serial.print("Heading: ");
+  Serial.println(headingDegrees);
+
+  client.publish("FIWARE/Wio_Link/Magnet",String(headingDegrees).c_str(),true);
+  delay(1000);
+}
