ce118ea0bbdf8b63245ec80b3254acf0247a9271
sach1n1 <sachin.s.dominic@gmail.com>
2020-07-28 16:36:52 +0200

    Update Hall_Sensor.cpp

--- Hall_Sensor.cpp
+++ Hall_Sensor.cpp
@@ -6,8 +6,8 @@
 #include <Wire.h>
 #include <SeeedOLED.h>
 
-const char* ssid = "FIWARE";
-const char* password =  "!FIWARE!on!air!";
+const char* ssid = "";
+const char* password =  "";
 #define mqtt_server "192.168.20.185"
 WiFiClient espClient;
 PubSubClient client(espClient);
