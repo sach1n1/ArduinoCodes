290f8130526f642759c667847a4086450c745ee9
sach1n1 <sachin.s.dominic@gmail.com>
2020-07-28 16:32:48 +0200

    Update compass.cpp

--- compass.cpp
+++ compass.cpp
@@ -9,8 +9,8 @@
 
 BMM150 bmm = BMM150();
 
-const char* ssid = "FIWARE";
-const char* password =  "!FIWARE!on!air!";
+const char* ssid = "";
+const char* password =  "";
 #define mqtt_server "192.168.20.185"
 WiFiClient espClient;
 PubSubClient client(espClient);
