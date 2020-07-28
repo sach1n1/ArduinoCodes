b17b333911827e08fe22072dfd5975eeecf2de12
sach1n1 <sachin.s.dominic@gmail.com>
2020-07-28 16:34:58 +0200

    Update PM2.5.cpp

--- PM2.5.cpp
+++ PM2.5.cpp
@@ -14,8 +14,8 @@
 #include <PubSubClient.h>
 #include <Wire.h>
 
-const char* ssid = "FIWARE";
-const char* password =  "!FIWARE!on!air!";
+const char* ssid = "";
+const char* password =  "";
 #define mqtt_server "192.168.20.185"
 WiFiClient espClient;
 PubSubClient client(espClient);
